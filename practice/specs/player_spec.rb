b3b4318d906453d6d2ff41d69644ba349ce17ec0
peaky76 <robertjamespeacock@gmail.com>
2020-06-24 19:54:15 +0100

    player initialized and tested

---
+++ practice/specs/player_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/reporters')
+
+require_relative('../player')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
+
+class TestPlayer < MiniTest::Test
+
+    def setup()
+        @player = Player.new("Dave")
+    end
+    
+    def test_player_has_name()
+        assert_equal("Dave", @player.name)
+    end
+
+    def test_player_has_lives()
+        assert_equal(6, @player.lives)
+    end
+
+end
